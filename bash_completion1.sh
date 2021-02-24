0d64d4651c8f929db26e9492986eba98c3ef1e36
Jesus Figueredo <jafigueredo@gmail.com>
2021-02-24 15:35:42 -0300

    tmp2

---
+++ bash_completion1.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+if ! command -v workspace &> /dev/null; then
+  return
+fi
+
+__autocomplection_workspace() {
+  if [ -z "$WORKSPACE_PATH" ]; then
+    WORKSPACE_PATH="$HOME/Work"
+  fi
+
+  command_named=$1
+  current_word_called=$2
+  word_before_called=$3
+
+  if [ "${#COMP_WORDS[@]}" = "2" ]; then
+
+    COMPREPLY=($(compgen -W "init i cd go path help --help -h" "${current_word_called}"))
+
+  elif [ "${#COMP_WORDS[@]}" = "3" ]; then
+
+    case $word_before_called in
+      cd | go | path ) COMPREPLY=($(compgen -W "`ls $WORKSPACE_PATH 2> /dev/null`" "${current_word_called}")) ;;
+      help    ) COMPREPLY=($(compgen -W "init i cd go" "${current_word_called}")) ;;
+    esac
+
+  elif [ "${#COMP_WORDS[@]}" = "4" ]; then
+
+    case "${COMP_WORDS[1]}" in
+      cd | go ) COMPREPLY=($(compgen -W "`ls "$WORKSPACE_PATH/$word_before_called" 2> /dev/null`" "${current_word_called}")) ;;
+    esac
+
+  fi
+
+  return 0
+}
+
+complete -o default -F __autocomplection_workspace workspace
+complete -o default -F __autocomplection_workspace wrk
